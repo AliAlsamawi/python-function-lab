5c99c0ad06fdb0a300445be4616534222779bb05
Ali A <alsamawiali4@gmail.com>
2021-11-16 15:05:31 -0500

    solve 3

--- python-function.py
+++ python-function.py
@@ -1,20 +1,25 @@
 # 1. Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n.
 
-def sum_to(n):
-  return sum(range(n+1))
+# def sum_to(n):
+#   return sum(range(n+1))
 
 # print(sum_to(6)) 
 # print(sum_to(10))
 
 # 2. Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
-def largest(nums):
-  return max(nums)
+# def largest(nums):
+#   return max(nums)
 
-print(largest([1, 2, 3, 4, 0]))
-print(largest([10, 4, 2, 231, 91, 54]))
+# print(largest([1, 2, 3, 4, 0]))
+# print(largest([10, 4, 2, 231, 91, 54]))
 
 # 3.Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
 
+def occurances(str1, str2):
+  print(str1.count(str2))
 
 
-
+print(occurances('fleep floop', 'e')) 
+# print(occurances('fleep floop', 'p'))
+# print(occurances('fleep floop', 'ee'))
+# print(occurances('fleep floop', 'fe'))
