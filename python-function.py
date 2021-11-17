3fe8cd6de8e4b9074ad3aacde7b85b39b1216aa9
Ali A <alsamawiali4@gmail.com>
2021-11-17 09:02:56 -0500

    4 done

--- python-function.py
+++ python-function.py
@@ -16,10 +16,21 @@
 # 3.Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
 
 def occurances(str1, str2):
-  print(str1.count(str2))
+  return(str1.count(str2))
 
 
 print(occurances('fleep floop', 'e')) 
-# print(occurances('fleep floop', 'p'))
-# print(occurances('fleep floop', 'ee'))
-# print(occurances('fleep floop', 'fe'))
+print(occurances('fleep floop', 'p'))
+print(occurances('fleep floop', 'ee'))
+print(occurances('fleep floop', 'fe'))
+
+
+# problem 4
+
+def product(*args):
+  product = 1
+  for a in args:
+      product *= a
+  return product
+
+print(product(-1, 4))
